68a3ad667c7203b2b0e4446409f637c39e583a63
SAGAR T <ssagu6661@gmail.com>
2024-08-21 13:43:27 -0400

    Add files via upload

---
+++ c17.py
@@ -0,0 +1,35 @@
+
+
+for i in range(11):
+ print("6 x",i,"=",6*i)   #   simple tables OUTPUT program
+   
+     
+print("\n")                                  
+num =int(input("Enter a number>>   ")) 
+for i in range(11):                           #  simple talbles INPUT program
+    print(f"{num} x {i} = {num*i}")
+     
+print("\n") 
+for i in range(num):
+          print(f"This number is made me repeat for {num} times")
+
+
+# def tables(num):
+#      for i in range(11):
+#           print(f"{num} x {i} = {num*i}") 
+          
+# def repeat(num):
+#      for i in range(num):
+#           print(f"this number is made me repeat for{num} times")
+          
+#           print("1.Tables \n2.Repeat")
+#           option = int(input("Option>>  "))
+#           num = int(input("Enter a number>> "))
+          
+#           if option ==1:
+#                tables(num)
+#           elif option == 2:
+#                repeat(num)
+#           else:
+#                print(" Option Invalid. Try Again!")
+          
